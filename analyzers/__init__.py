e2108f2f2ae75048737e34f19d0070d8c1af18c9
poorvak <poorvak.kapoor@gmail.com>
2016-04-07 20:03:33 +0530

    conde changes from static method

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -1,16 +1,23 @@
-"""
-Suggest Analyzer and Auto Complete Filter Class
-"""
+"""Suggest Analyzer and Auto Complete Filter Class."""
 from elasticsearch_dsl import analyzer, token_filter
 
 other_param = dict()
 
 
 class Suggest(object):
+    """
+    Suggest class.
+
+    usage example:
+        >> suggest_obj = Suggest()
+        >> suggestion = suggest_obj.suggest()
+        >> autocomplete = suggest_obj.autocomplete()
+    """
+
     other_param.update({"min_gram": 1, "max_gram": 20})
 
-    @staticmethod
     def suggest():
+        """Suggest method which used edge n-gram analyzer."""
         suggestfilter = token_filter("suggestfilter", type="ngram",
                                      **other_param)
 
@@ -19,11 +26,10 @@ class Suggest(object):
                                        "asciifolding"])
         return bestsuggest
 
-    @staticmethod
     def autocomplete():
+        """Autocomplete method uses n-gram analyzer."""
         autocompletefilter = token_filter("autocompletefilter",
                                           type="edge_ngram", **other_param)
         autocomplete = analyzer("autocomplete", tokenizer="standard",
                                 filter=["lowercase", autocompletefilter])
         return autocomplete
-
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+"""Setup file."""
 from setuptools import setup
 
+
 setup(name='analyzers',
-      version='0.1',
+      version='0.1.1',
       description='Analysers for elasticsearch for best suggestion and auto complete',
       url='https://github.com/Poorvak/analyzers',
       author='Poorvak Kapoor',
